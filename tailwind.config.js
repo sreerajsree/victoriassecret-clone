f72efd85faa42e7204462c338e688fae7cfd30a7
sreerajsree <sreeandsreeraj@gmail.com>
2022-09-30 23:51:02 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bg-primary': '#F9E2E7',
+      },
+    },
+  },
+  plugins: [],
+}
