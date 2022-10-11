d2205106553eb2779a1077cc9e413c14d46140b1
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-11 19:14:15 +0530

    data

--- components/NavItems.jsx
+++ components/NavItems.jsx
@@ -1,96 +1,21 @@
 import React from "react";
-import Link from "next/link";
+import DATA from "../data/data.json";
+import SubMenu from "./SubMenu";
 
 const NavItems = () => {
   return (
     <>
       <nav className="hidden md:block">
         <div className="max-w-5xl mx-auto my-5 h-5">
-          <ul className="uppercase text-sm sm:text-xs font-thin opacity-80 flex items-center justify-between tracking-wider">
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  new!
+          <ul className="uppercase text-sm font-thin opacity-80 flex items-center justify-between tracking-wider">
+            {DATA.nav.map((item, key) => (
+              <li key={key} className="hoverable hover:bg-[#F7F7F8] hover:text-black">
+                <a className="relative block hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4 cursor-pointer">
+                  {item.name}
                 </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  bras
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  panties
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  lingerie
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  sleep
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  sport & lounge
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  beauty
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  accessories
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  swim
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  vsco-lab
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  sale
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/#">
-                <a className="hover:underline underline-offset-[18px] decoration-[#F4C6CF] decoration-4">
-                  vs now
-                </a>
-              </Link>
-            </li>
+                <SubMenu sub={item.submenu} />
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
