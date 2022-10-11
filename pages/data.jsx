424b0a60ce56e7a249ddd20b0a75c752f5ae4519
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-11 17:39:10 +0530

    data

---
+++ pages/data.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+
+const data = () => {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 4 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+  return (
+    <Carousel
+    className='container mx-auto my-20'
+    swipeable={false}
+    draggable={false}
+    showDots={false}
+    responsive={responsive}
+    ssr={false} // means to render carousel on server-side.
+    infinite={false}
+    keyBoardControl={true}
+    containerClass="carousel-container"
+    removeArrowOnDeviceType={["tablet", "mobile"]}
+    dotListClass="custom-dot-list-style"
+    itemClass="carousel-item-padding-40-px"
+  >
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+    <div className='p-2'>
+        <img src="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="apple" className='w-full' />
+    </div>
+  </Carousel>
+  )
+}
+
+export default data
