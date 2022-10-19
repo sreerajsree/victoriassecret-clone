51d40996bfd6ab663ddeab0d23c26c0652fdbb44
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-20 00:35:18 +0530

    footer

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Image from "next/image";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 const Accordion = () => {
   const [accordion, setAccordion] = useState(0);
@@ -7,38 +9,127 @@ const Accordion = () => {
       <button
         onClick={() => (accordion === 1 ? setAccordion(0) : setAccordion(1))}
         type="button"
-        className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500"
+        className="flex items-center justify-between w-full p-5 text-left text-gray-500 font-semibold uppercase text-sm border-b border-gray-300"
       >
-        <span>What is Flowbite?</span>
-        <svg
-          className={`w-6 h-6 shrink-0 ${
-            accordion === 1 ? "rotate-180" : "rotate-360"
-          }`}
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <span>
+          <div className="flex divide-x divide-gray-300">
+            <button className="px-2">
+              <Image
+                className="rounded-full"
+                src="/india.png"
+                width={16}
+                height={16}
+                alt="india"
+              />
+            </button>
+            <button className="uppercase px-2">english</button>
+            <button className="uppercase px-2">inr</button>
+          </div>
+        </span>
+        {accordion === 1 ? (
+          <AiOutlineMinus className="w-5 h-5" />
+        ) : (
+          <AiOutlinePlus className="w-5 h-5" />
+        )}
       </button>
       {accordion === 1 ? (
         <div>
           <div className="p-5 font-light border border-b-0 border-gray-200">
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Data 1
+            <p className="text-xs text-gray-600">
+              Your order will be charged and processed in Indian Rupee (INR)
             </p>
-            <p className="text-gray-500">
-              Data Accordion
-            </p>
+            <select
+              id="countries"
+              class="bg-[#F5F5F6] text-gray-600 text-sm block w-full p-2.5 outline-none mt-2"
+            >
+              <option selected>India</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+            <select
+              id="countries"
+              class="bg-[#F5F5F6] text-gray-600 text-sm block w-full p-2.5 outline-none mt-3"
+            >
+              <option selected>Choose a country</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+            <button className="border border-black rounded w-full py-3 px-8 uppercase mt-4">save</button>
           </div>
         </div>
       ) : (
         ""
       )}
+      <button
+        onClick={() => (accordion === 2 ? setAccordion(0) : setAccordion(2))}
+        type="button"
+        className="flex items-center justify-between w-full p-5 text-left text-gray-500 font-bold uppercase text-sm"
+      >
+        <span>help</span>
+        {accordion === 2 ? (
+          <AiOutlineMinus className="w-5 h-5" />
+        ) : (
+          <AiOutlinePlus className="w-5 h-5" />
+        )}
+      </button>
+      {accordion === 2 ? (
+        <div>
+          <div className="p-5 font-light border border-b-0 border-gray-200 text-gray-600 text-sm">
+            <ul>
+              <li className="mb-3">Customer Service</li>
+              <li className="mb-3">Live Chat</li>
+              <li className="mb-3">Careers</li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <button
+        onClick={() => (accordion === 3 ? setAccordion(0) : setAccordion(3))}
+        type="button"
+        className="flex items-center justify-between w-full p-5 text-left text-gray-500 font-bold uppercase text-sm"
+      >
+        <span>orders & returns</span>
+        {accordion === 3 ? (
+          <AiOutlineMinus className="w-5 h-5" />
+        ) : (
+          <AiOutlinePlus className="w-5 h-5" />
+        )}
+      </button>
+      {accordion === 3 ? (
+        <div>
+          <div className="p-5 font-light border border-b-0 border-gray-200 text-gray-600 text-sm">
+            <ul>
+              <li className="mb-3">Order Status</li>
+              <li className="mb-3">Shipping Information</li>
+              <li className="mb-3">Return Policy</li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="p-2">
+      <div className="my-8 text-[10px] text-gray-500 container mx-auto">
+          <p className="border-b border-gray-300 pb-5">© 2022 Victoria&apos;s Secret. All Rights Reserved.</p>
+          <div className="flex pt-5">
+            <div className="pr-1">Terms of Use</div>
+            <div className="border-l border-black px-1">Privacy & Security</div>
+            <div className="border-l border-black px-1">
+              Modern Slavery Transport Statement
+            </div>
+            <div className="border-l border-black px-1">Ad Peferences</div>
+            <div className="border-l border-black px-1">
+              SSL Secure Checkout
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,22 @@ import { AiFillFacebook, AiFillInstagram, AiFillYoutube } from "react-icons/ai";
 import { BsTwitter, BsPinterest } from "react-icons/bs";
 import Accordion from "./Accordion";
 
+const Data = [
+  "today's offers",
+  "new!",
+  "bras",
+  "panties",
+  "lingerie",
+  "sleep",
+  "sport & lounge",
+  "beauty",
+  "accesories",
+  "swim",
+  "vsco-lab",
+  "sale",
+  "vs now",
+];
 const Footer = () => {
-  
   return (
     <>
       <div className="hidden md:block">
@@ -53,6 +67,41 @@ const Footer = () => {
         </div>
       </div>
       <div className="block md:hidden">
+        {Data.map((item, key) => (
+          <button
+            key={key}
+            type="button"
+            className="flex items-center justify-between w-full p-4 text-left text-gray-500 font-semibold uppercase text-sm"
+          >
+            <span>{item}</span>
+            <svg
+              className="w-6 h-6 shrink-0 -rotate-90"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        ))}
+        <div className="bg-[#F5F5F6] py-5">
+          <div className="text-center text-gray-800">
+            <button className="rounded uppercase font-semibold mx-auto w-auto py-4 px-8 border border-black bg-white">
+              sign up to stay in the know
+            </button>
+          </div>
+          <div className="flex mt-5 ml-7">
+            <AiFillFacebook className="w-6 h-6" />
+            <BsTwitter className="w-5 h-5 ml-5" />
+            <AiFillYoutube className="h-6 w-6 ml-5" />
+            <BsPinterest className="h-5 w-5 ml-5" />
+            <AiFillInstagram className="h-6 w-6 ml-5" />
+          </div>
+        </div>
         <Accordion/>
       </div>
     </>
